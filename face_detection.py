ec865c4705254709f9537360cfbd0c654a64f83e
ngopal561998 <ngopal561998@gmail.com>
2021-03-07 14:36:37 +0530

    first commit

---
+++ face_detection.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('C:\\Users\\nanda gopal\\Desktop\\face detection\\haarcascade_frontalface_default.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('C:\\Users\\nanda gopal\\Desktop\\face detection\\haarcascade_eye.xml')
+
+#cap = cv2.VideoCapture(0)
+
+
+img=cv2.imread("C:\\Users\\nanda gopal\\Desktop\\face detection\\group_pic.jfif")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+
+
+        cv2.imshow('img',img)
+        cv2.waitKey(100000)
+
+
